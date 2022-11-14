2158463466e068dbf8cbdc015889c15bf9b780f7
shubham negi <40260256+shubhamnegi2@users.noreply.github.com>
2022-11-14 20:54:16 +0530

    Add files via upload

--- cuteEdgeTask/js/index.js
+++ cuteEdgeTask/js/index.js
@@ -24,21 +24,43 @@ $(function () {
 
   $(window).scroll(function () {
     var scrollTopValue = $(window).scrollTop();
-    console.log(scrollTopValue, typeof scrollTopValue)
+    console.log(scrollTopValue, typeof scrollTopValue);
     if (scrollTopValue > 0) {
       $("header").css({
         backgroundColor: "#1d1d1b",
       });
       $(".headerColors").css({
-        display:'flex',
+        display: "flex",
       });
     } else {
       $("header").css({
         backgroundColor: "transparent",
       });
       $(".headerColors").css({
-        display:'none',
-      });      
+        display: "none",
+      });
     }
+  });
+
+  $(".ourClints-slider").owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplaySpeed: 3000,
+    slideTransition: "linear",
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 6,
+      },
+    },
   });
 });
